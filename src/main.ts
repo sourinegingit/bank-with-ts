9002061282901cca55cfa02dabcd99fefb806790
negin <neginsouri7979@gmail.com>
2024-11-23 11:49:02 +0430

    create random number for create account number pass and cardnumber

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,23 @@
 import './style.css'
 
-console.log("Hello, TypeScript!");
+interface User{
+  firstName: string;
+  lastName: string;
+  mobileNumber: string;
+  email: string;
+  dob: Date;
+}
+interface Transaction {
+  fromAccount: string;
+    toAccount: string;
+    amount: number;
+    transactionDate: Date;
+}
+
+
+// ایجاد شماره حساب و پسورد و شماره کارت رندوم 
+const generateRandomNumber=(length:number):string=>{
+  return Math.random().toString().slice(2,2+length);
+}
+const generateAccountNumber = (): string => generateRandomNumber(10);
+console.log(generateAccountNumber());
